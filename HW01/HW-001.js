93fd97aace5785dae262b84e3343dc4c47ffeeb1
Eugenia Lime <EugeniaLime@Eugenia-Lime.local>
2021-09-27 18:37:12 +0300

    .gitignore file is updated with .DS_store file

--- HW01/HW-001.js
+++ HW01/HW-001.js
@@ -4,45 +4,35 @@ const priceOceanPlastic = 90.2345;
 
 // Task#1
 const maxPrice = Math.max(priceJellyfish, priceWhale, priceOceanPlastic);
-console.log("Найвища ціна: " + maxPrice + " грн.");
 
 // Task#2
 const minPrice = Math.min(priceJellyfish, priceWhale, priceOceanPlastic);
-console.log("Найнижча ціна: " + minPrice + " грн.");
 
 // Task#3
 const totalPrice = priceJellyfish + priceWhale + priceOceanPlastic;
-console.log("Загальна вартість товарів: " + totalPrice + " грн.");
 
 // Task#4
 const totalRoundedPrice = Math.trunc(priceJellyfish) + Math.trunc(priceWhale) + Math.trunc(priceOceanPlastic);
-console.log("Сума вартостей (без копійок): " + totalRoundedPrice + " грн");
 
 // Task#5
 const totalRoundToHundretsPrice = (Math.round(totalPrice / 100) * 100);
-console.log("Сума товарів округлена до сотень: " + totalRoundToHundretsPrice + " грн.");
 
 // Task#6
 const isEvenNumber = (Math.trunc(totalPrice) % 2 === 0);
-console.log("Сума товарів, округлена в меншу сторону, є парним числом: " + isEvenNumber);
 
 // Task#7
 const clientMoney = 500;
 const clientChange = clientMoney - totalPrice;
-console.log("Решта при оплаті купюрою " + clientMoney + " грн: " + clientChange + " грн.");
 
 // Task#8
 const averagePriceValue = totalPrice / 3;
 const roundedAveragePrice = Math.round(averagePriceValue * 100) / 100;
-console.log("Середнє значення цін: " + roundedAveragePrice + " грн.");
 
 // Task#9
 const randomDiscount = Math.floor(Math.random() * 101);
-const priceWithDiscount = (totalPrice * (100 - randomDiscount) / 100).toFixed(2);
-console.log("Сума до оплати, з урахуванням випадкової знижки: " + priceWithDiscount + " грн.");
+const priceWithDiscount = +(totalPrice * (100 - randomDiscount) / 100).toFixed(2);
 
 const netProfit = priceWithDiscount - totalPrice / 2;
-console.log("Чистий прибуток: " + netProfit + " грн.");
 
 document.writeln(
     `

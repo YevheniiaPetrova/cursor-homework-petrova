b803d6a42775a3459fa3d1f52a7281f523cd57a6
Eugenia Lime <EugeniaLime@Eugenia-Lime.local>
2021-11-09 20:53:34 +0200

    HW#13 refactored

--- HW13/script.js
+++ HW13/script.js
@@ -1,18 +1,18 @@
 const getRandomChinese = async (length) => {
 
-    const result = [];
+    const result = '';
 
     while (length) {
         const promise = new Promise((resolve, reject) => {
             setTimeout(() => {
-                const sign = Date.now().toString().split('').splice(-5, 5).join('');
+                const sign = Date.now().toString().slice(-5);
                 resolve(String.fromCharCode(sign));
             }, 50);
         });
 
         try {
             const randomChinese = await promise;
-            result.push(randomChinese);
+            result += randomChinese;
         } catch (error) {
             console.log('Error: ', error);
         }
@@ -20,7 +20,7 @@ const getRandomChinese = async (length) => {
         length--;
     }
     
-    console.log(result.join(''));
+    console.log(result);
 }
 
 getRandomChinese(4);
